441c5b77bc6eb7bd236686ef97b35d27879c8119
miguelaponce <miguel@rondayapp.com>
2024-01-23 19:01:38 -0800

    .

--- src/collision.rs
+++ src/collision.rs
@@ -1,26 +1,40 @@
 use bevy::{
-    ecs::{
-        component::Component,
-        entity::Entity,
-        query::{With, Without},
-        system::Query,
-    },
+    ecs::{component::Component, entity::Entity, query::Without, system::Query},
     math::Vec2,
     prelude::{Children, Rect},
-    transform::components::{Transform, GlobalTransform},
+    transform::components::{GlobalTransform, Transform},
     utils::{HashMap, HashSet},
 };
 
 use crate::{
-    debug_bounding_box::{get_transform, Boundry, WallType},
+    debug_bounding_box::{Boundry, WallType},
     Floor, Player,
 };
+
+pub struct CollidingSides {
+    pub top: bool,
+    pub bottom: bool,
+    pub left: bool,
+    pub right: bool,
+}
+
+impl CollidingSides {
+    pub fn new() -> Self {
+        Self {
+            top: false,
+            bottom: false,
+            left: false,
+            right: false,
+        }
+    }
+}
 
 #[derive(Component)]
 pub struct Collider {
     pub radius: f32,
     pub collider_entities: HashSet<Entity>,
     pub is_grounded: bool,
+    pub is_colliding: CollidingSides,
     pub rect: Rect,
     pub offset: Vec2,
 }
@@ -33,6 +47,7 @@ impl Collider {
             is_grounded: false,
             rect,
             offset,
+            is_colliding: CollidingSides::new(),
         }
     }
 }
@@ -43,10 +58,10 @@ pub fn floor_collision(
     children: Query<(&Boundry, &GlobalTransform)>,
 ) {
     for (_, mut player_collider, _, p_children) in &mut player {
-        player_collider.collider_entities.clear();
+        // clear collided sides
+        player_collider.is_colliding = CollidingSides::new();
 
         let mut p_boundry_map = HashMap::new();
-
         for &i in p_children {
             if let Ok((c_boundry, c_trans)) = children.get(i) {
                 p_boundry_map.insert(c_boundry.wall_type.clone(), c_trans);
@@ -54,13 +69,17 @@ pub fn floor_collision(
         }
 
         for (_, _, _, f_children) in &floor_query {
-            let mut f_boundry_map = HashMap::new();
 
+            let mut f_boundry_map = HashMap::new();
             for &i in f_children {
                 if let Ok((f_boundry, f_trans)) = children.get(i) {
                     f_boundry_map.insert(f_boundry.wall_type.clone(), f_trans);
                 }
             }
+
+            let Some(p_top) = p_boundry_map.get(&WallType::Top) else {
+                continue;
+            };
 
             let Some(p_bottom) = p_boundry_map.get(&WallType::Bottom) else {
                 continue;
@@ -78,6 +97,10 @@ pub fn floor_collision(
                 continue;
             };
 
+            let Some(f_bottom) = f_boundry_map.get(&WallType::Bottom) else {
+                continue;
+            };
+
             let Some(f_left) = f_boundry_map.get(&WallType::Left) else {
                 continue;
             };
@@ -86,18 +109,51 @@ pub fn floor_collision(
                 continue;
             };
 
-            
-            if p_right.translation().x > f_left.translation().x && p_left.translation().x < f_right.translation().x && (p_bottom.translation().y - 20.) <= f_top.translation().y {
-                player_collider.is_grounded = true;
-            } else {
-                player_collider.is_grounded = false;
+            if p_right.translation().x > f_left.translation().x
+                && p_left.translation().x < f_right.translation().x
+                && !player_collider.is_colliding.bottom
+            {
+                if (p_bottom.translation().y - 20.) <= f_top.translation().y {
+                    player_collider.is_colliding.bottom = true;
+                } else {
+                    player_collider.is_colliding.bottom = false;
+                }
             }
 
-            // if colliding left 
-             
-            // if colliding top 
-            
+            if p_right.translation().x > f_left.translation().x
+                && p_left.translation().x < f_right.translation().x
+                && !player_collider.is_colliding.top
+            {
+                if p_top.translation().y >= f_bottom.translation().y {
+                    player_collider.is_colliding.top = true;
+                } else {
+                    player_collider.is_colliding.top = false;
+                }
+            }
+
             // if colliding right
+            if p_top.translation().y > f_bottom.translation().y
+                && p_bottom.translation().y < f_top.translation().y 
+                && !player_collider.is_colliding.right
+            {
+                if p_right.translation().x <= f_left.translation().x {
+                    player_collider.is_colliding.right = true;
+                } else {
+                    player_collider.is_colliding.right = false;
+                }
+            }
+
+            // if colliding left
+            if p_top.translation().y > f_bottom.translation().y
+                && p_bottom.translation().y < f_top.translation().y
+                && !player_collider.is_colliding.left
+            {
+                if p_left.translation().x >= f_right.translation().x {
+                    player_collider.is_colliding.left = true;
+                } else {
+                    player_collider.is_colliding.left = false;
+                }
+            }
         }
     }
 }
